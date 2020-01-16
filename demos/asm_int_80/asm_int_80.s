75e85e8987f4945c872d2bd879e2d8e8b4086935
wangli <wangli@tencent.com>
2020-01-16 19:14:37 +0800

    asm int 80

---
+++ demos/asm_int_80/asm_int_80.s
@@ -0,0 +1,12 @@
+global _start
+_start:
+	mov dword edx, 13
+	mov dword ecx, message
+	mov dword ebx, 1
+	mov dword eax, 4
+	int 0x80
+	mov dword ebx, 0
+	mov dword eax, 1
+	int 0x80
+message:
+        db      "Hello, World", 10 
