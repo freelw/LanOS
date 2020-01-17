57703c34aa258c0231468ed6bf22643fccb7a078
wangli <liwang112358@gmail.com>
2020-01-17 21:19:06 +0800

    cpp

---
+++ demos/protect_mode_cpp_demo/lan_main.cpp
@@ -0,0 +1,37 @@
+extern "C" void write_char(char ch);
+class L
+{
+public:
+	L() {
+	    write_char('L');
+	}
+};
+class O: public L
+{
+public:
+	O() {
+	    write_char('O');
+	}
+};
+
+class V: public O
+{
+public:
+	V() {
+	    write_char('V');
+	}
+};
+
+class E: public V
+{
+public:
+	E() {
+	    write_char('E');
+	}
+};
+
+extern "C" void lan_main()
+{
+	E e;
+	while(1);
+}
