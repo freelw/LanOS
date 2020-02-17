dddee25b69be789af4cdedd075dbdef360057d54
liwang <liwang112358@gmail.com>
2020-02-17 17:14:12 +0800

    fix loop times because of bios rom shadow

--- demos/fs_demo/fs_patch_tool/patch.js
+++ demos/fs_demo/fs_patch_tool/patch.js
@@ -33,13 +33,21 @@ async function main() {
     }
     console.log('fs_header length : ', fs_header.length);
 
-    const page_dir_start = 0x8000;
+    /*const page_dir_start = 0x8000;
     const header_start_pos = page_dir_start - 120;
     console.log('header_start_pos : ', header_start_pos);
     const buffer_0 = zero_buffer(120-fs_header.length);
     const buffer_before_fs_header = vfd_content.slice(0, header_start_pos);
     const buffer_after_fs_header = vfd_content.slice(page_dir_start);
-    const final_buffer = Buffer.concat([buffer_before_fs_header, fs_header, buffer_0, buffer_after_fs_header]);
+    const final_buffer = Buffer.concat([buffer_before_fs_header, fs_header, buffer_0, buffer_after_fs_header]);*/
+
+    const start = 0x8ffff;
+    //const start = 0xee7ff;
+    const p0 = vfd_content.slice(0, start);
+    const p1 = zero_buffer(1);
+    p1.writeUInt8(0x22);
+    const p2 = vfd_content.slice(start+1);
+    const final_buffer = Buffer.concat([p0, p1, p2]);
     fs.writeFileSync(vfd_path, final_buffer);
     process.exit();
 }
