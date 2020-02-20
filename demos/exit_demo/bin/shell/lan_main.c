0984f37a6e00106fd0d389e6f5a989aa64065335
liwang <liwang112358@gmail.com>
2020-02-20 23:18:00 +0800

    exit demo start

---
+++ demos/exit_demo/bin/shell/lan_main.c
@@ -0,0 +1,128 @@
+#define _syscall0(type,name) \
+type name(void) \
+{ \
+long __res; \
+__asm__ volatile ("int $0x80" \
+	: "=a" (__res) \
+	: "0" (__NR_##name)); \
+if (__res >= 0) \
+	return (type) __res; \
+return -1; \
+}
+
+#define _syscall1(type,name,atype,a) \
+type name(atype a) \
+{ \
+long __res; \
+__asm__ volatile ("int $0x80" \
+	: "=a" (__res) \
+	: "0" (__NR_##name),"b" ((long)(a))); \
+if (__res >= 0) \
+	return (type) __res; \
+return -1; \
+}
+
+#define _syscall2(type,name,atype,a,btype,b) \
+type name(atype a,btype b) \
+{ \
+long __res; \
+__asm__ volatile ("int $0x80" \
+	: "=a" (__res) \
+	: "0" (__NR_##name),"b" ((long)(a)),"c" ((long)(b))); \
+if (__res >= 0) \
+	return (type) __res; \
+return -1; \
+}
+
+#define _syscall3(type,name,atype,a,btype,b,ctype,c) \
+type name(atype a,btype b,ctype c) \
+{ \
+long __res; \
+__asm__ volatile ("int $0x80" \
+	: "=a" (__res) \
+	: "0" (__NR_##name),"b" ((long)(a)),"c" ((long)(b)),"d" ((long)(c))); \
+if (__res>=0) \
+	return (type) __res; \
+return -1; \
+}
+
+int s_print_str(char*);
+int s_print_num(int);
+int s_get_keyboad_buffer(char*);
+int equal_buffer(char *a, char *b, int len);
+int s_clean_keyboard();
+int s_exec(char*);
+int s_print_shell(char*);
+int fork1();
+
+void lan_main()
+{
+	s_print_str("--------------------------");
+	s_print_str("|                        |");
+	s_print_str("| welcome to lan_os 0.1! |");
+	s_print_str("|                        |");
+	s_print_str("--------------------------");
+    s_print_str("shell started.");
+    char buffer0[256];
+    char buffer1[256];
+	for (int i = 0; i < 256; ++ i) {
+		buffer0[i] = buffer1[i] = 0;
+	}
+    while (1) {
+        s_get_keyboad_buffer(buffer0);
+        if (0 != equal_buffer(buffer0, buffer1, 256)) {
+			int enter = 0;
+			for (int i = 0; i < 256; ++ i) {
+				if (buffer0[i] == 'E') {
+					enter = 1;
+					buffer0[i] = 0;
+					break;
+				}
+			}
+			if (enter) {
+				s_clean_keyboard();
+				if (!fork1()) {
+					s_exec(buffer0);
+					s_print_str("no exec file");
+					while(1);
+				}
+			}
+			s_print_shell(buffer0);
+        }
+        for (int i = 0; i < 256; ++ i) {
+            buffer1[i] = buffer0[i];
+        }
+    }
+}
+
+int equal_buffer(char *a, char *b, int len)
+{
+	for (int i = 0; i < len; ++ i) {
+		if (a[i] != b[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+#define __NR_test_sys_call 0
+#define __NR_fork1 1
+#define __NR_test_sys_call1 2
+#define __NR_test_sys_call2 3
+#define __NR_s_print_str 4
+#define __NR_s_print_num 5
+#define __NR_s_read_file_content 6
+#define __NR_s_exec 7
+#define __NR_s_get_keyboad_buffer 8
+#define __NR_s_clean_keyboard 9
+#define __NR_s_print_shell 10
+_syscall0(int, test_sys_call)
+_syscall0(int, fork1)
+_syscall0(int, test_sys_call2)
+_syscall1(int, s_print_str, char*, msg)
+_syscall1(int, s_print_num, int, num)
+_syscall2(int, s_read_file_content, char*, file_name, char*, buffer)
+_syscall1(int, s_exec, char*, file_name)
+_syscall1(int, s_get_keyboad_buffer, char*, buffer)
+_syscall0(int, s_clean_keyboard)
+_syscall1(int, s_print_shell, char*, buffer)
---
+++ demos/exit_demo/keyboard.c
@@ -0,0 +1,136 @@
+#include "gate_tool.h"
+
+extern void _keyboard_interrupt();
+extern void print_str(char *s);
+extern void print_num(int num);
+extern void print_hex(int num);
+extern void panic(char *msg);
+
+#define outb(value,port) \
+__asm__ ("outb %%al,%%dx"::"a" (value),"d" (port))
+
+
+#define inb(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al":"=a" (_v):"d" (port)); \
+_v; \
+})
+
+#define outb_p(value,port) \
+__asm__ ("outb %%al,%%dx\n" \
+		"\tjmp 1f\n" \
+		"1:\tjmp 1f\n" \
+		"1:"::"a" (value),"d" (port))
+
+#define inb_p(port) ({ \
+unsigned char _v; \
+__asm__ volatile ("inb %%dx,%%al\n" \
+	"\tjmp 1f\n" \
+	"1:\tjmp 1f\n" \
+	"1:":"=a" (_v):"d" (port)); \
+_v; \
+})
+
+struct pair
+{
+    unsigned char code;
+    char ch;
+};
+
+#define CODE_MAP_LEN 40
+#define CODES_LEN 256
+
+int input_code_index;
+char codes[CODES_LEN];
+struct pair code_map[CODE_MAP_LEN] = {
+    {0x2, '1'},
+    {0x3, '2'},
+    {0x4, '3'},
+    {0x5, '4'},
+    {0x6, '5'},
+    {0x7, '6'},
+    {0x8, '7'},
+    {0x9, '8'},
+    {0xa, '9'},
+    {0xb, '0'},
+    {0x10, 'q'},
+    {0x11, 'w'},
+    {0x12, 'e'},
+    {0x13, 'r'},
+    {0x14, 't'},
+    {0x15, 'y'},
+    {0x16, 'u'},
+    {0x17, 'i'},
+    {0x18, 'o'},
+    {0x19, 'p'},
+    {0x1e, 'a'},
+    {0x1f, 's'},
+    {0x20, 'd'},
+    {0x21, 'f'},
+    {0x22, 'g'},
+    {0x23, 'h'},
+    {0x24, 'j'},
+    {0x25, 'k'},
+    {0x26, 'l'},
+    {0x2c, 'z'},
+    {0x2d, 'x'},
+    {0x2e, 'c'},
+    {0x2f, 'v'},
+    {0x30, 'b'},
+    {0x31, 'n'},
+    {0x32, 'm'},
+    {0x1c, 'E'},
+};
+
+void get_codes(char *buffer)
+{
+    for (int i = 0; i < CODES_LEN; ++ i) {
+        buffer[i] = 0;
+    }
+    for (int i = 0; i < input_code_index; ++ i) {
+        buffer[i] = codes[i];
+    }
+}
+
+void parse_code(unsigned char code)
+{
+    if (code == 0x0e) {
+        if (input_code_index > 0) {
+            codes[input_code_index--] = 0;
+        }
+        return ;
+    }
+    for (int i = 0; i < CODE_MAP_LEN; ++ i) {
+        if (code == code_map[i].code) {
+            codes[input_code_index++] = code_map[i].ch;
+        }
+    }
+}
+
+void keyboard_interrupt()
+{
+    unsigned char code = inb_p(0x60);
+    parse_code(code);
+    outb_p(0x20, 0x20);
+}
+
+void clean_keyboard_buffer()
+{
+    input_code_index = 0;
+    for (int i = 0; i < CODES_LEN; ++ i) {
+        codes[i] = 0;
+    }
+}
+
+void init_keyboard()
+{
+    input_code_index = 0;
+    for (int i = 0; i < CODES_LEN; ++ i) {
+        codes[i] = 0;
+    }
+    set_trap_gate(0x21,&_keyboard_interrupt);
+	outb_p(inb_p(0x21)&0xfd,0x21);
+	unsigned char a=inb_p(0x61);
+	outb_p(a|0x80,0x61);
+	outb_p(a,0x61);
+}
