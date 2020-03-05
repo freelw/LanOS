db1b9c010678b5101d5754cdbd17e98f83e15779
liwang <liwang112358@gmail.com>
2020-03-05 09:58:24 +0800

    update

--- demos/exit_demo/bin/test/lan_main.c
+++ demos/exit_demo/bin/test/lan_main.c
@@ -1,3 +1,14 @@
+#define _syscall0(type,name) \
+type name(void) \
+{ \
+long __res; \
+__asm__ volatile ("int $0x80" \
+	: "=a" (__res) \
+	: "0" (__NR_##name)); \
+if (__res >= 0) \
+	return (type) __res; \
+return -1; \
+}
 #define _syscall1(type,name,atype,a) \
 type name(atype a) \
 { \
@@ -9,6 +20,7 @@ if (__res >= 0) \
 	return (type) __res; \
 return -1; \
 }
+int s_exit();
 
 int s_print_str(char*);
 int s_print_num(int);
@@ -18,9 +30,11 @@ void lan_main()
     s_print_str("I am a test executable file !");
     s_print_str("I can print a number.");
     s_print_num(222);
-    while(1);
+    s_exit();
 }
 #define __NR_s_print_str 4
 #define __NR_s_print_num 5
+#define __NR_s_exit 12
 _syscall1(int, s_print_str, char*, msg)
 _syscall1(int, s_print_num, int, num)
+_syscall0(int, s_exit)
