ea82ad6bfe6a6085a0242c230a5e2a64940d490b
wangli <wangli@tencent.com>
2021-01-05 23:51:38 +0800

    update

--- demos/common_header_demo/bin/loop/lan_main.c
+++ demos/common_header_demo/bin/loop/lan_main.c
@@ -3,7 +3,7 @@
 void lan_main() {
   s_print_str("start printing 20 times.");
   s_print_crlf();
-  for (int i = 0; i < 20; ++ i) {
+  for (int i = 0; i < 200; ++ i) {
     s_print_str("im a loop.");
     s_print_num(i);
     s_print_crlf();
---
+++ demos/common_header_demo/bin/readme.md
@@ -0,0 +1,6 @@
+# bin目录下二进制文件的编译方法
+
+以loop为例
+首先要source根目录下的 lan_os_rc
+然后 cd loop
+执行 lan_make
